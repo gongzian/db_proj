043c93c7f232fc336e989a187e001af4d37af28a
Zian Gong <gongzian@msu.edu>
2021-12-04 12:31:09 -0500

    Initial commit

---
+++ src/main/java/com/example/test/mapper/UserMapper.java
@@ -0,0 +1,19 @@
+package com.example.test.mapper;
+
+import com.example.test.bean.UserBean;
+
+import java.util.List;
+
+public interface UserMapper {
+    UserBean getInfo(String name,String password);
+
+    UserBean selectUserById(String id);
+
+    int insertUser(UserBean userBean);
+
+    int deleteUser(String id);
+
+    int updateUser(UserBean userBean);
+
+    List<UserBean> getAllUser();
+}
